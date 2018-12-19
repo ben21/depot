81bbc67a992fcfaed4728839a7dfb39ddae292fb
Benjamin <bvanbever1@gmail.com>
2018-12-19 16:36:56 +0100

    Changed pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ This book will help you get there with a real-world introduction to seven platfo
     whether you’re new to mobile or an experienced developer needing to expand your options.
     Plus, you’ll find out which cross-platform solution makes the most sense for your needs. </p>
 },
-image_url: '7apps.jpg',
+image_url: 'SPONSORED The Electrifying Aretha Franklin - SEALED 1962 US 1st Press Stereo CS 8561.jpg',
 price: 26.00)
